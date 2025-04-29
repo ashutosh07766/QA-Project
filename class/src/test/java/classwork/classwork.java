9940bc70a9a2562ad8da32ca2184b871af960609
Ashutosh Kumar Singh <ashuhar9450@gmail.com>
2025-04-30 02:59:26 +0530

    Add files

---
+++ class/src/test/java/classwork/classwork.java
@@ -0,0 +1,35 @@
+package classwork;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class classwork {
+	WebDriver driver;
+
+    @BeforeClass
+    public void openBrowser() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.apple.com/in/");
+    }
+
+    @Test
+    public void verifyTitle() {
+        String expected = "Apple";
+        String actual = driver.getTitle();
+        assert actual.contains(expected) : "Title check failed!";
+        System.out.println("Page title: " + actual);
+    }
+
+    @AfterClass
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+}
